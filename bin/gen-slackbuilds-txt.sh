5b98f73930f97c9415d22ba0bb9d2a857d053fef
AitorATuin <atuin.dev@gmail.com>
2016-03-20 00:33:00 +0100

    Added more checks

--- bin/gen-slackbuilds-txt.sh
+++ bin/gen-slackbuilds-txt.sh
@@ -10,26 +10,31 @@ function line() {
     echo "SLACKBUILD $1: $2"
 }
 
+function error() {
+    echo "ERROR: $1: $2"
+}
+
 rm -rf ${DEST_FILE}
 
 for pkg_info in $(find . -name "*.info"); do
     pkg_dir=`dirname ${pkg_info}`
+    pkg_section=${pkg_dir##/*}
     pkg_desc=${pkg_dir}/slack-desc
     pkg_info_basename=$(basename ${pkg_info})
     pkg_name=${pkg_info_basename%%.*}
     echo "Processing package ${pkg_name}"
     [ ! -f ${pkg_info} ] && {
-        echo "Ignoring package ${pkg_name}. Missing ${pkg_info}"
+        error ${pkg_section} "Missing ${pkg_info}"
         continue
     }
     [ ! -f ${pkg_desc} ] && {
-        echo "Ignoring package ${pkg_name}. Missing ${pkg_desc}"
+        error ${pkg_section} "Missing ${pkg_desc}"
         continue
     }
     description=$(cat ${pkg_desc} | grep ${pkg_name} | head -1 |cut -d " " -f 3-)
     source ${pkg_info}
     [ "${PRGNAM}" != "${pkg_name}" ] && {
-        echo "Ignoring package ${pkg_name}: PRGNAM ${PRGNAM} != ${pkg_name}"
+        error ${pkg_section} "PRGNAM ${PRGNAM} != ${pkg_name}"
         continue
     }
     line "NAME" ${PRGNAM} >> ${DEST_FILE}
